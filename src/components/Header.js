5d685bb2c57bd020c98147e0fada66fce8dd1906
Harry Ganz <hganz@harryganz.com>
2016-08-04 19:50:31 -0400

    Makes react app boilerplate

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header (props) {
+  return (
+    <div className='page-header'>
+      <h1>M&#252;vieHa&#252;s</h1>
+    </div>
+  );
+}
+
+module.exports = Header;
