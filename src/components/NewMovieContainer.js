0b5105098b156083c55dec35e0c98804b3309964
Harry Ganz <harry.y.ganz@gmail.com>
2016-08-05 16:31:08 -0400

    Use react-redux to connect store to new movie view

--- src/components/NewMovieContainer.js
+++ src/components/NewMovieContainer.js
@@ -1,42 +1,24 @@
-import React, {Component} from 'react';
-import store from '../stores/movie-store.js';
-import NewMovie from './NewMovie';
-import Loading from './Loading';
-import ErrorMessage from './ErrorMessage';
+import {connect} from 'react-redux';
+import {fetchNewMovie} from '../actions/new-movie.js';
+import NewMovieView from './NewMovieView';
 
-class NewMovieContainer extends Component {
-  constructor (props) {
-    super(props);
-    this.state = store.getState();
-
-    this.unsubscribe = null;
-    this._onChange = this._onChange.bind(this);
-  }
-
-  componentDidMount () {
-    this.unsubscribe = store.subscribe(this._onChange);
-  }
-
-  componentWillUnmount () {
-    this.unsubscribe();
-  }
-
-  _onChange () {
-    this.setState(store.getState());
-  }
-
-  render () {
-    if (this.state.error) {
-      return <ErrorMessage error={this.state.error} />;
-    } else if (this.state.isFetching) {
-      return <Loading />;
-    } else if (this.state.hasMovie) {
-      let {title, rating, year} = this.state.movie;
-      return <NewMovie title={title} rating={rating} year={year} />;
-    } else {
-      return <div></div>;
-    }
-  }
+function mapStateToProps (state) {
+  let {error, isFetching, hasMovie} = state;
+  let {title, year, rating} = state.movie;
+  return {
+    error: error,
+    isFetching: isFetching,
+    hasMovie: hasMovie,
+    title: title,
+    year: year,
+    rating: rating
+  };
 }
 
-module.exports = NewMovieContainer;
+function mapDispatchToProps (dispatch) {
+  return {
+    search: (title) => { dispatch(fetchNewMovie(title)); }
+  };
+}
+
+module.exports = connect(mapStateToProps, mapDispatchToProps)(NewMovieView);
--- src/components/NewMovieFormContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {dispatch} from '../stores/movie-store.js';
-import {fetchNewMovie} from '../actions/new-movie.js';
-import NewMovieForm from './NewMovieForm';
-
-module.exports = function () {
-  return <NewMovieForm search={ (title) => { dispatch(fetchNewMovie(title)); } } />;
-};
