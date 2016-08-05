306421e376a0a4ae9384463ae105403b97c231c8
Harry Ganz <harry.y.ganz@gmail.com>
2016-08-05 16:30:10 -0400

    Adds store provider to app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './stores/movie-store.js';
 import App from './components/App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
